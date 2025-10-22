dce3ebcbefe0f2f0f484b770503019802145196e
maxfieldsmith <maxfieldsmith@pointb.com>
2025-10-21 19:48:06 -0700

    Get my Greenbox for the day

--- code/2. Remove Element/remove.py
+++ code/2. Remove Element/remove.py
@@ -15,6 +15,6 @@ class Solution(object):
         for num in nums:
             if num != val:
                 nums[not_val] = num
-                not_val += 1 # 
+                not_val += 1 # update where to place next non_val element
 
         return not_val
